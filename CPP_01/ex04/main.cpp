06cc03e0d7111fda2a8d67cc7ba43b75e3196630
avg38 <mailamandineg@gmail.com>
2024-06-12 16:33:40 +0200

    update

--- CPP_01/ex04/main.cpp
+++ CPP_01/ex04/main.cpp
@@ -1,9 +1,27 @@
-#include <fstream>  // Pour std::ifstream
-#include <iostream> // Pour std::cerr
-#include <sstream>  // Pour std::stringstream
-#include <string>   // Pour std::string
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
 
 int	main(int ac, char **av)
 {
+	if (ac != 4)
+	{
+		std::cerr << "Error wrong number of arguments" << std::endl;
+		std::cerr << "Format expected: ./sed <filename> <old_word> <new_word >" <<std::endl;
+		return 1;
+	}
+	std::ifstream infile(av[1]);
 	
+	if (!infile.is_open())
+	{
+		std::cerr << 'Error: ' << av[1] << ": infile can't be opened" << std::endl;
+		return 1;
+	}
+
+	std::stringstream	buffer;
+	buffer << infile.rdbuf();
+	infile.close()
+
+
 }
